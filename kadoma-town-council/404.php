250e649f2eefc5ba9e126d95f20aba17bdf8b3fa
stanysaizi <stanysaizi4@gmail.com>
2025-04-30 08:21:55 +0200

    Add kadoma-town-council/404.php 1745994113858

---
+++ kadoma-town-council/404.php
@@ -0,0 +1,13 @@
+<?php
+include 'includes/header.php';
+?>
+
+<section class="mt-10 max-w-5xl mx-auto text-center">
+    <h2 class="text-5xl font-bold mb-4">404</h2>
+    <p class="text-xl mb-6">Oops! The page you are looking for cannot be found.</p>
+    <a href="index.php" class="text-blue-700 hover:underline text-lg">Return to Home</a>
+</section>
+
+<?php
+include 'includes/footer.php';
+?>
