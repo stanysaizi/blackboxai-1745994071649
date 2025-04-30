3dc7f4ffb17931fc6cb2d91dfc1b1d9c5abcd95a
stanysaizi <stanysaizi4@gmail.com>
2025-04-30 08:21:26 +0200

    Add kadoma-town-council/includes/footer.php 1745994085558

---
+++ kadoma-town-council/includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+// Common footer include for Kadoma Town Council website
+?>
+</main>
+
+<footer class="bg-white mt-20 py-6 shadow-inner text-center text-gray-600">
+    &copy; 2024 Kadoma Town Council. All rights reserved.
+</footer>
+
+</body>
+</html>
